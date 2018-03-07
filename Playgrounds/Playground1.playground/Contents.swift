5ed4adb636387e5e551235150debf20abe597502
Vadym Markov <markov.vadym@gmail.com>
2018-03-07 21:40:12 +0100

    Add cover and icon images

--- Playgrounds/Playground1.playground/Contents.swift
+++ Playgrounds/Playground1.playground/Contents.swift
@@ -5,6 +5,16 @@ import PlaygroundSupport
 import RetroProgress
 
 let progressView = ProgressView(frame: CGRect(x: 0, y: 0, width: 360, height: 30))
-progressView.animateProgress(to: 0.9)
+
+// Configure
+progressView.progressInset = .init(top: 4, left: 4, bottom: 4, right: 4)
+progressView.layer.cornerRadius = 0
+progressView.layer.borderColor = UIColor.white.cgColor
+progressView.trackColor = .black
+progressView.progressColor = .white
+progressView.separatorColor = .black
+
+// Set progress
+progressView.animateProgress(to: 0.9, duration: 5)
 
 PlaygroundPage.current.liveView = progressView
